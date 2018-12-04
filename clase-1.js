351caebbd62d5a4cff16d7f406e83d50a7a0b6d8
Arnold Restrepo Hernandez <restrepo_arnold@outlook.com>
2018-12-04 16:49:59 -0500

    Variables

---
+++ clase-1.js
@@ -0,0 +1,8 @@
+// Imprimir mensaje en pantalla
+console.log("Hola Mundo js")
+
+const nombre = 'Arnold Michel',
+      apellido = 'Restrepo Hernández',
+      edad = 21  
+
+console.log(`Hola ${nombre} ${apellido} \n Tengo ${edad} años`)
