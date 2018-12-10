bd3653cd3274ed99849b608a9cb0afdb1a5631be
Arnold Restrepo Hernandez <restrepo_arnold@outlook.com>
2018-12-10 17:04:00 -0500

    Numbers Operations & Decimals

---
+++ clase-3-numbers.js
@@ -0,0 +1,18 @@
+let year = 21,
+    weight = 59,
+    lastYear = 19,
+    newYear,
+    priceWine = 300.3
+    totalWine = priceWine * 3
+    totalWineClient = priceWine * 100 * 3 / 100
+
+
+// Add Years year = year + 1
+year += 1
+weight -= 2
+
+newYear = year += lastYear
+
+
+
+
