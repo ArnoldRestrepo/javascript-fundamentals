0295a743d33121957a54852e896790783bfc62f3
Arnold Restrepo Hernandez <restrepo_arnold@outlook.com>
2018-12-10 16:45:14 -0500

    Strings Methods, chartAt, substr, String Interpolation

--- clase-2-strings.js
+++ clase-2-strings.js
@@ -1,5 +1,14 @@
-var nombre = "Arnold Restrepo"
+var nombre = "Arnold",
+    apellido = 'Restrepo'
 
-var nombreEnMayusculas = nombre.toUpperCase()
+var nombreEnMayusculas = nombre.toUpperCase(),
+    nombreEnMinusculas = nombre.toLocaleLowerCase()
+    primeraLetra = nombre.charAt(0),
+    ultimaLetra = nombre.substr(-1),
+    cantidadDeLetrasNombre = nombre.length,
+    nombreCompleto = `${nombre} ${apellido}`,
+    substraerDosLetras = nombre.substr(0,1)
 
 console.log(nombreEnMayusculas)
+console.log(nombreEnMinusculas)
+console.log(nombreCompleto)
