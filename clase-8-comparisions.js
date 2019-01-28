8cbe31d50d934a63a5a72dbe3ed6ea11f2bd996c
Arnold Restrepo <restrepo_arnold@outlook.com>
2019-01-27 20:46:14 -0500

    Comparaciones en JavaScript

---
+++ clase-8-comparisions.js
@@ -0,0 +1,28 @@
+/*
+Boolean:// false/true
+Null:// nulo o "vacío"
+Undefined:// Cuando una variable es declarada pero aún no tiene un valor asignado 
+Number:// 383839
+String:// "esto es una string"```
+*/
+
+const x = 4,
+    y = '4'
+
+const user1 = {
+    name: 'Arnold'
+}
+// false
+
+const user2 = {
+    name: 'Dario'
+}
+// false
+
+const user3 = {
+    ...user2
+}
+// false
+
+const user4 = user3
+
