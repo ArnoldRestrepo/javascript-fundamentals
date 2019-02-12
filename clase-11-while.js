45583d343c9eca58d253b6eb9530fa721628aab7
Arnold Restrepo Hernandez <restrepo_arnold@outlook.com>
2019-02-12 15:51:25 -0500

    While Loop

--- clase-11-while.js
+++ clase-11-while.js
@@ -6,8 +6,8 @@ const PERSON_ONE = {
 }
 
 const WEIGHT_VALUE = 0.2
-const YEAR_DAYS = 365
-
+let yearDaysStart = 0
+const GOAL = PERSON_ONE.weight - 3
 /**
  * @name IncreaseWeight
  * @param Object
@@ -24,9 +24,39 @@ const IncreaseWeight = (person) => person.weight += WEIGHT_VALUE
 
 const DecreaseWeight = (person) => person.weight -= WEIGHT_VALUE
 
+/**
+ * @name Eat
+ * @returns Random Number
+ * @description Validate if user eat a lot food
+ */
+
+const Eat = () => Math.random() < 0.3
+
+/**
+ * @name Run
+ * @returns Random Number
+ * @description Validate if the user does running
+ */
+
+const Run = () => Math.random() < 0.4
+
 console.log(`Al inicio de año ${PERSON_ONE.name} pesaba ${PERSON_ONE.weight}kg`)
 
+while(PERSON_ONE.weight > GOAL){
+    if(Eat()){
+        // aumentar peso
+        IncreaseWeight(PERSON_ONE)
+    } 
+    
+    if (Run()) {
+        // dismininuir peso
+        DecreaseWeight(PERSON_ONE)
+    }
+    yearDaysStart++
+}
 
 
-console.log(`Al final de año ${PERSON_ONE.name} pesa ${PERSON_ONE.weight}kg`)
+// console.log(`Al final de año ${PERSON_ONE.name} pesa ${PERSON_ONE.weight}kg`)
+console.log(`Pasaron ${yearDaysStart} días hasta que  ${PERSON_ONE.name} adelgazo 3kg`)
+
 
