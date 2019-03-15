6c1e5d81fcc33c658831c18c3919ba822c4efd09
Arnold Restrepo <restrepo_arnold@outlook.com>
2019-03-15 16:39:44 -0500

    Callback Hell

--- clase-23-callbacks.js
+++ clase-23-callbacks.js
@@ -3,13 +3,18 @@ const PEOPLE_URL = 'people/:id'
 const PLANET_URL = 'planet/:id'
 const OPTIONS_URL = { crossDomain: true }
 
-const onPeopleResponse = (luke) => { console.log(`Hola, yo soy, ${luke.name}`) }
 
-const getActor = (id) => {
+const getActor = (id , callback) => {
     const URL = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-    $.get(URL, OPTIONS_URL, onPeopleResponse)
+    $.get(URL, OPTIONS_URL, (opts) => { 
+        console.log(`Hola, yo soy, ${opts.name}`)
+
+        if(callback){ callback() }
+    })
 }
 
-getActor(1);
-getActor(7);
-getActor(5);
+getActor(1, () => {
+    getActor(7, () => {
+        getActor(5);
+    });
+});
