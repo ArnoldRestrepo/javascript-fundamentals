a298abb84e87776687c957303714195c2e2e5124
Arnold Restrepo <restrepo_arnold@outlook.com>
2019-03-19 17:01:12 -0500

    Class Car for Example Attributes

--- clase-20-class-ecmascript.js
+++ clase-20-class-ecmascript.js
@@ -29,4 +29,38 @@ class Developer extends Person{
 }
 
 
+class Car {
+    constructor(model, price, color, type){
+        this.model = model
+        this.price = price
+        this.color = color
+        this.type = type
+    }
+
+    chooseCar(){
+        switch (this.model.toLowerCase()) {
+            case 'mazda':
+                console.log('Your Cars is Mazda')
+                break;
+            case 'chevrolet':
+                console.log('Your Cars is Chevrolet')
+                break;
+            case 'bmw':
+                console.log('Your Cars is BMW')
+                break;
+            default:
+                console.log('The choose car is incorrect!')    
+                break;
+        }
+    }
+
+    buyCar(){
+        console.log(`The car choose is: ${this.model}, your price: ${this.price}, your color: ${this.color} and type is ${this.type}`)
+    }
+}
+
+const mazda = new Car('audi', 3400, 'red', 'cupper')
+
+mazda.buyCar()
+mazda.chooseCar()
 
