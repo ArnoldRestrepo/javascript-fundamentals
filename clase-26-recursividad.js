00021dc2933f7afe61eab5caa518abfe1fb90f10
Arnold Restrepo <restrepo_arnold@outlook.com>
2019-03-29 14:03:52 -0500

    Recursividad

---
+++ clase-26-recursividad.js
@@ -0,0 +1,16 @@
+/**
+ * @name divisionEntera
+ * @param {*} dividendo 
+ * @param {*} divisor
+ * @description 
+ *  La recursividad es un concepto muy importante en cualquier lenguaje de programación. Una función recursiva es básicamente aquella que se llama (o se ejecuta) a sí misma de forma controlada, hasta que sucede una condición base.
+ */
+
+const divisionEntera = (dividendo, divisor) => {
+    if(dividendo < divisor) {
+        return 0
+    } 
+
+    return 1 + divisionEntera(dividendo - divisor, divisor)
+}
+
